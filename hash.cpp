dc0bd785d3727115713cda7cecb8884f4bd561bb
yousafmuse <yousufmuse@gmail.com>
2021-12-05 15:01:44 +0500

    Add project files.

---
+++ hash.cpp
@@ -0,0 +1,71 @@
+
+#include "hash.h"
+#include "math.h"
+HashTable::HashTable(unsigned size, hash_fn hash){
+    
+}
+
+HashTable::~HashTable(){
+    
+}
+
+File HashTable::getFile(string name, unsigned int diskBlock){
+    
+}
+
+bool HashTable::insert(File file){
+    
+}
+
+bool HashTable::remove(File file){
+    
+}
+
+float HashTable::lambda(TABLENAME tablename) const {
+      
+}
+
+float HashTable::deletedRatio(TABLENAME tableName) const {
+    
+}
+
+void HashTable::dump() const {
+    cout << "Dump for table 1: " << endl;
+    if (m_table1 != nullptr)
+        for (int i = 0; i < m_capacity1; i++) {
+            cout << "[" << i << "] : " << m_table1[i] << endl;
+        }
+    cout << "Dump for table 2: " << endl;
+    if (m_table2 != nullptr)
+        for (int i = 0; i < m_capacity2; i++) {
+            cout << "[" << i << "] : " << m_table2[i] << endl;
+        }
+}
+
+bool HashTable::isPrime(int number){
+    bool result = true;
+    for (int i = 2; i <= number / 2; ++i) {
+        if (number % i == 0) {
+            result = false;
+            break;
+        }
+    }
+    return result;
+}
+
+int HashTable::findNextPrime(int current){
+    //we always stay within the range [MINPRIME-MAXPRIME]
+    //the smallest prime starts at MINPRIME
+    if (current < MINPRIME) current = MINPRIME-1;
+    for (int i=current; i<MAXPRIME; i++) { 
+        for (int j=2; j*j<=i; j++) {
+            if (i % j == 0) 
+                break;
+            else if (j+1 > sqrt(i) && i != current) {
+                return i;
+            }
+        }
+    }
+    //if a user tries to go over MAXPRIME
+    return MAXPRIME;
+}
